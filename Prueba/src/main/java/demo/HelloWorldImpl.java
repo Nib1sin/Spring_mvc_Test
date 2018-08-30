001e201ba2ef77e8aa8b8a7910bd76ee3a2e8a7b
Nibi <nibi.amdukia@gmail.com>
2018-08-30 10:45:02 +0200

    Hello_World

---
+++ Prueba/src/main/java/demo/HelloWorldImpl.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class HelloWorldImpl implements HelloWorldService{
+    public void sayHello(){
+        System.out.println("Hello World!!!!");
+    }
+}
